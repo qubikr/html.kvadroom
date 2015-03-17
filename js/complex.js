9819e9c80d4d5722d9e2951884eadab5d443ad5d
ruslanchek <rshashkov@icloud.com>
2015-03-17 12:52:39 +0300

    Maps

--- js/complex.js
+++ js/complex.js
@@ -1,45 +1,69 @@
 var Complex = {};
 
-Complex.SliderGallery = function(){
-    var $container = $('.slider-gallery'),
-        $frame = $container.find('.frame');
+// Complex.SliderGallery = function(){
+//     var $container = $('.slider-gallery'),
+//         $frame = $container.find('.frame');
 
+//     this.init = function(){
+//         $frame.sly({
+//             horizontal: 1,
+//             itemNav: 'basic',
+//             smart: 1,
+//             activateOn: 'click',
+//             mouseDragging: 1,
+//             touchDragging: 1,
+//             releaseSwing: 1,
+//             forceCenter: 1,
+//             centered: 1,
+//             startAt: 1,
+//             scrollBy: 1,
+//             activatePageOn: 'click',
+//             speed: 700,
+//             elasticBounds: 1,
+//             easing: 'easeOutExpo',
+//             dragHandle: 1,
+//             dynamicHandle: 1,
+//             clickBar: 1,
+//             prevPage: $container.find('.left'),
+//             nextPage: $container.find('.right')
+//         });
+
+//         return this;
+//     };
+
+//     this.resize = function(){
+//         $frame.sly('reload');
+//     };
+// };
+
+Complex.Map = function(){
     this.init = function(){
-        $frame.sly({
-            horizontal: 1,
-            itemNav: 'basic',
-            smart: 1,
-            activateOn: 'click',
-            mouseDragging: 1,
-            touchDragging: 1,
-            releaseSwing: 1,
-            forceCenter: 1,
-            centered: 1,
-            startAt: 1,
-            scrollBy: 1,
-            activatePageOn: 'click',
-            speed: 700,
-            elasticBounds: 1,
-            easing: 'easeOutExpo',
-            dragHandle: 1,
-            dynamicHandle: 1,
-            clickBar: 1,
-            prevPage: $container.find('.left'),
-            nextPage: $container.find('.right')
+        var data = $('.complex-map').data();
+        var map = new GeoMap({
+            id: 'complex-map',
+            center: data.center,
+            onInit: function(instance){
+                var area = new map.Area({
+                    coords: data.area,
+                    content: data.title, 
+                    onBalloonReady: function(){
+                        area.showBalloon(true);
+                    }
+                });
+
+                area.pointMap();
+            }
         });
 
+        map.init();
+
         return this;
-    };
-
-    this.resize = function(){
-        $frame.sly('reload');
-    };
-
-    return this;
+    };  
 };
 
 Complex.init = function(){
-    this.sliderGallery = new this.SliderGallery().init();
+    // this.sliderGallery = new this.SliderGallery().init();
+    this.Map = new this.Map().init();
 };
 
 $(function(){
---
+++ js/maps.js
@@ -0,0 +1,145 @@
+var GeoMap = function(opts){
+	var _this = this,
+		map,
+		options = $.extend({
+			id: 'map',
+			center: [55.76, 37.64],
+			zoom: 10,
+			layer: 'map',
+			onInit: function(){}
+		}, opts);
+
+	var draw = function(){
+		map = new ymaps.Map(options.id, {
+	        center: options.center,
+	        zoom: options.zoom
+	    });
+	};
+
+	var Balloon = function(center, content, onReady){
+		var _this = this,
+			id = _.uniqueId('balloon'),
+			$element = $(),
+			permanent = false,
+			marker = new ymaps.Placemark(center, {
+		        iconContent: '<div class="map-balloon disabled" id="' + id + '"><div class="inner">' + content + '</div></div>'
+		    }, {
+		        iconImageHref: '',
+		        iconImageSize: 20,
+		        iconImageOffset: 10,
+		        zIndex: 2
+		    });
+
+	    marker.events.add('overlaychange', function(){
+	    	$element = $('#' + id);
+
+			$element.css({
+		    	marginTop: -$element.height()/2
+		    });
+
+		    if(onReady) onReady();
+	    });
+
+	    map.geoObjects.add(marker);
+
+	    var hideOnClick = function(){
+    		_this.hide();
+	    };
+
+	    this.show = function(permanentShow){
+	    	$element.removeClass('disabled');
+	    	map.events.add('click', hideOnClick);
+
+	    	if(permanent !== true){
+	    		permanent = permanentShow;
+	    	}
+	    };
+
+	    this.hide = function(){
+	    	if(permanent !== true){
+		    	$element.addClass('disabled');
+		    	map.events.remove('click', hideOnClick);
+		    }
+	    };
+	};
+
+	this.Area = function(opts){
+		var options = $.extend({
+				coords: [[
+					[55.75, 37.80],
+	                [55.80, 37.90],
+	                [55.75, 38.00],
+	                [55.70, 38.00],
+	                [55.70, 37.80]
+	            ]],
+				content: '', 
+				onReady: function(){}, 
+				onBalloonReady: function(){}
+			}, opts);
+
+		var	area = new ymaps.GeoObject({
+	        geometry: {
+	            type: 'Polygon',
+	            coordinates: options.coords,
+	            fillRule: 'nonZero'
+		        }
+		    }, {
+		        fillColor: 'rgba(255, 0, 0, 0.1)',
+		        strokeColor: 'rgba(255, 0, 0, 1)',
+		        opacity: 1,
+		        strokeWidth: 2
+	    	});		
+
+		map.geoObjects.add(area);
+
+		var bounds = area.geometry.getBounds(),
+			center = [];
+
+		center[0] = bounds[0][0] + ((bounds[1][0] - bounds[0][0]) / 2);
+		center[1] = bounds[0][1] + ((bounds[1][1] - bounds[0][1]) / 2);
+
+		var balloonCoords = [center[0], bounds[1][1]];
+
+		console.log(center, balloonCoords, bounds[1][1]);
+
+		var balloon = new Balloon(balloonCoords, options.content, options.onBalloonReady)
+		
+		area.events.add('click', function(){
+			map.panTo(center, {
+				delay: 0
+			});
+
+			balloon.show();
+		});
+
+		area.events.add('overlaychange', function(){
+			if(options.onReady) options.onReady();
+		});
+
+		this.pointMap = function(){
+			map.setCenter(center);
+		};
+
+		this.showBalloon = function(permanent){
+			balloon.show(permanent);
+		};
+
+		this.hideBalloon = function(){
+			balloon.hide();
+		};
+	};
+
+	this.init = function(){
+        if(typeof ymaps != 'undefined'){
+        	draw();
+        	options.onInit(_this);
+        }else{
+            $.getScript("http://api-maps.yandex.ru/2.0-stable/?load=package.full&lang=ru-RU", function() {
+                ymaps.ready(function(){
+        			draw();
+        			options.onInit(_this);
+                });
+            });
+        }
+    };
+};
