1f6c81e38544e1f970d2c1b3d72ea1b402905bb2
ruslanchek <rshashkov@icloud.com>
2015-03-26 12:12:22 +0300

    Auth fixed

--- server.js
+++ server.js
@@ -6,12 +6,17 @@ var md5 = require('MD5');
 var app = express();
 
 app
-	.use(serveStatic(__dirname + '/public'))
 	.use(basicAuth(function(credentials, req, res, next) {
 		var key = '7f58b698ca4937b2e3df346979f0f5e8';
-		var result = (key == md5(md5(user)) && key == md5(md5(pass)));
+		var result = (key == md5(md5(credentials.username)) && key == md5(md5(credentials.password)));
 
-		next();
+		if(result){
+			app.use(serveStatic(__dirname + '/public'));
+			
+			next();
+		}else{
+			return res.status(500).send('Password wrong!');
+		}
 	 
 	}, 'Please enter your credentials.'));
 
