24594378c2d62d92acecdb7845f58a9b5bc9c5ab
ruslanchek <rshashkov@icloud.com>
2015-03-26 11:53:33 +0300

    Password, Empty section

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var md5 = require('MD5');
 
-connect().use(serveStatic(__dirname)).listen(process.env.PORT || 5000);
+connect()
+	.use(connect.basicAuth(function(user, pass){
+		var key = '7f58b698ca4937b2e3df346979f0f5e8';
+		
+		return key == md5(md5(user)) && key == md5(md5(pass));
+	}))
+	.use(serveStatic(__dirname))
+	.listen(process.env.PORT || 5000);
