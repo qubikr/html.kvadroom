9f91f4aed95307ee682200b4f3472bef72531182
Дмитрий <dmitriy@iMac-Alexey-Lipskerov.local>
2015-05-29 11:18:27 +0300

    fix input ENTRY bloc width

--- public/js/jquery.KVMainSearchString.js
+++ public/js/jquery.KVMainSearchString.js
@@ -2,17 +2,17 @@
 
     var returnData = '[{"title":"\u0416\u041a \u00ab\u041f\u0435\u0440\u0432\u044b\u0439 \u041c\u043e\u0441\u043a\u043e\u0432\u0441\u043a\u0438\u0439\u00bb <em>\u041c\u043e\u0441\u043a\u043e\u0432\u0441\u043a\u0438\u0439<\/em>","type":"city","wagons":[{"id":"252482","type":"city","data":{"title":"\u0416\u041a \u00ab\u041f\u0435\u0440\u0432\u044b\u0439 \u041c\u043e\u0441\u043a\u043e\u0432\u0441\u043a\u0438\u0439\u00bb"}}]},{"title":"\u0416\u041a \u00ab\u041d\u043e\u0432\u043e-\u041c\u043e\u043b\u043e\u043a\u043e\u0432\u043e\u00bb <em>\u041b\u0435\u043d\u0438\u043d\u0441\u043a\u0438\u0439 \u0440\u0430\u0439\u043e\u043d<\/em>","type":"city","wagons":[{"id":"252461","type":"city","data":{"title":"\u0416\u041a \u00ab\u041d\u043e\u0432\u043e-\u041c\u043e\u043b\u043e\u043a\u043e\u0432\u043e\u00bb"}}]},{"title":"\u0416\u041a \u00ab\u0413\u0440\u0430\u0434 \u041c\u043e\u0441\u043a\u043e\u0432\u0441\u043a\u0438\u0439\u00bb <em>\u041c\u043e\u0441\u043a\u043e\u0432\u0441\u043a\u0438\u0439<\/em>","type":"city","wagons":[{"id":"252391","type":"city","data":{"title":"\u0416\u041a \u00ab\u0413\u0440\u0430\u0434 \u041c\u043e\u0441\u043a\u043e\u0432\u0441\u043a\u0438\u0439\u00bb"}}]},{"title":"\u041c\u043e\u0436\u0430\u0439\u0441\u043a <em>\u041c\u043e\u0436\u0430\u0439\u0441\u043a\u0438\u0439 \u0440\u0430\u0439\u043e\u043d<\/em>","type":"city","wagons":[{"id":"3934","type":"city","data":{"title":"\u041c\u043e\u0436\u0430\u0439\u0441\u043a"}}]},{"title":"\u0434. \u041c\u043e\u0441\u043a\u0432\u043e\u0440\u0435\u0446\u043a\u0430\u044f \u0421\u043b\u043e\u0431\u043e\u0434\u0430 <em>\u041c\u043e\u0436\u0430\u0439\u0441\u043a\u0438\u0439 \u0440\u0430\u0439\u043e\u043d<\/em>","type":"city","wagons":[{"id":"13312","type":"city","data":{"title":"\u0434. \u041c\u043e\u0441\u043a\u0432\u043e\u0440\u0435\u0446\u043a\u0430\u044f \u0421\u043b\u043e\u0431\u043e\u0434\u0430"}}]},{"title":"\u041c\u043e\u0436\u0430\u0439\u0441\u043a <em>\u0411\u0435\u043b\u043e\u0440\u0443\u0441\u0441\u043a\u043e\u0435 \u043d\u0430\u043f\u0440.<\/em>","type":"railway","wagons":[{"id":"61","type":"railway","data":{"title":"\u041c\u043e\u0436\u0430\u0439\u0441\u043a","group_id":"1190"}}]},{"title":"\u041c\u043e\u0441\u043a\u043e\u0432\u0441\u043a\u0438\u0439 <em>\u041d\u043e\u0432\u043e\u043c\u043e\u0441\u043a\u043e\u0432\u0441\u043a\u0438\u0439 \u0410\u041e<\/em>","type":"city","wagons":[{"id":"7170","type":"city","data":{"title":"\u041c\u043e\u0441\u043a\u043e\u0432\u0441\u043a\u0438\u0439"}}]},{"title":"\u043f. \u0417\u0430\u0432\u043e\u0434 \u041c\u043e\u0441\u0440\u0435\u043d\u0442\u0433\u0435\u043d <em>\u041d\u043e\u0432\u043e\u043c\u043e\u0441\u043a\u043e\u0432\u0441\u043a\u0438\u0439 \u0410\u041e<\/em>","type":"city","wagons":[{"id":"12826","type":"city","data":{"title":"\u043f. \u0417\u0430\u0432\u043e\u0434 \u041c\u043e\u0441\u0440\u0435\u043d\u0442\u0433\u0435\u043d"}}]},{"title":"\u0441. \u041c\u043e\u043a\u0440\u043e\u0435 <em>\u041c\u043e\u0436\u0430\u0439\u0441\u043a\u0438\u0439 \u0440\u0430\u0439\u043e\u043d<\/em>","type":"city","wagons":[{"id":"13310","type":"city","data":{"title":"\u0441. \u041c\u043e\u043a\u0440\u043e\u0435"}}]},{"title":"\u0421\u041d\u0422 \u00ab\u041c\u043e\u0441\u043a\u043e\u0432\u0441\u043a\u0438\u0439 \u0422\u0440\u0443\u0431\u043d\u044b\u0439 \u0437\u0430\u0432\u043e\u0434\u00bb <em>\u041d\u043e\u0432\u043e\u043c\u043e\u0441\u043a\u043e\u0432\u0441\u043a\u0438\u0439 \u0410\u041e<\/em>","type":"city","wagons":[{"id":"256650","type":"city","data":{"title":"\u0421\u041d\u0422 \u00ab\u041c\u043e\u0441\u043a\u043e\u0432\u0441\u043a\u0438\u0439 \u0422\u0440\u0443\u0431\u043d\u044b\u0439 \u0437\u0430\u0432\u043e\u0434\u00bb"}}]},{"title":"\u041c\u043e\u0436\u0430\u0439\u0441\u043a \u0433\u043f <em>\u041c\u043e\u0436\u0430\u0439\u0441\u043a\u0438\u0439 \u0440\u0430\u0439\u043e\u043d<\/em>","type":"city","wagons":[{"id":"10499","type":"city","data":{"title":"\u041c\u043e\u0436\u0430\u0439\u0441\u043a \u0433\u043f"}}]},{"title":"\u0441. \u041c\u043e\u043b\u043e\u043a\u043e\u0432\u043e <em>\u041b\u0435\u043d\u0438\u043d\u0441\u043a\u0438\u0439 \u0440\u0430\u0439\u043e\u043d<\/em>","type":"city","wagons":[{"id":"12825","type":"city","data":{"title":"\u0441. \u041c\u043e\u043b\u043e\u043a\u043e\u0432\u043e"}}]},{"title":"\u0434. \u041c\u043e\u0434\u0435\u043d\u043e\u0432\u043e <em>\u041c\u043e\u0436\u0430\u0439\u0441\u043a\u0438\u0439 \u0440\u0430\u0439\u043e\u043d<\/em>","type":"city","wagons":[{"id":"13309","type":"city","data":{"title":"\u0434. \u041c\u043e\u0434\u0435\u043d\u043e\u0432\u043e"}}]},{"title":"\u0434. \u041c\u043e\u0440\u0434\u0432\u0438\u043d\u043e\u0432\u043e <em>\u041c\u043e\u0436\u0430\u0439\u0441\u043a\u0438\u0439 \u0440\u0430\u0439\u043e\u043d<\/em>","type":"city","wagons":[{"id":"13311","type":"city","data":{"title":"\u0434. \u041c\u043e\u0440\u0434\u0432\u0438\u043d\u043e\u0432\u043e"}}]},{"title":"\u0434. \u041c\u043e\u0442\u044f\u0433\u0438\u043d\u043e <em>\u041c\u043e\u0436\u0430\u0439\u0441\u043a\u0438\u0439 \u0440\u0430\u0439\u043e\u043d<\/em>","type":"city","wagons":[{"id":"13313","type":"city","data":{"title":"\u0434. \u041c\u043e\u0442\u044f\u0433\u0438\u043d\u043e"}}]},{"title":"\u0441. \u041c\u043e\u0447\u0438\u043b\u044b <em>\u0421\u0435\u0440\u0435\u0431\u0440\u044f\u043d\u043e-\u041f\u0440\u0443\u0434\u0441\u043a\u0438\u0439 \u0440\u0430\u0439\u043e\u043d<\/em>","type":"city","wagons":[{"id":"15520","type":"city","data":{"title":"\u0441. \u041c\u043e\u0447\u0438\u043b\u044b"}}]},{"title":"\u043f\u0433\u0442. \u041c\u043e\u043d\u0438\u043d\u043e <em>\u0429\u0435\u043b\u043a\u043e\u0432\u0441\u043a\u0438\u0439 \u0440\u0430\u0439\u043e\u043d<\/em>","type":"city","wagons":[{"id":"16832","type":"city","data":{"title":"\u043f\u0433\u0442. \u041c\u043e\u043d\u0438\u043d\u043e"}}]},{"title":"\u0411\u0426 \u00ab\u041c\u043e\u0436\u0430\u0439\u0441\u043a\u0438\u0439 (B)\u00bb <em>\u0417\u0430\u043f\u0430\u0434\u043d\u044b\u0439 \u0410\u041e<\/em>","type":"city","wagons":[{"id":"254993","type":"city","data":{"title":"\u0411\u0426 \u00ab\u041c\u043e\u0436\u0430\u0439\u0441\u043a\u0438\u0439 (B)\u00bb"}}]},{"title":"\u0421\u041d\u0422 \u00ab\u0421\u0435\u0440\u043f \u0438 \u041c\u043e\u043b\u043e\u0442\u00bb <em>\u0414\u043e\u043c\u043e\u0434\u0435\u0434\u043e\u0432\u043e<\/em>","type":"city","wagons":[{"id":"256708","type":"city","data":{"title":"\u0421\u041d\u0422 \u00ab\u0421\u0435\u0440\u043f \u0438 \u041c\u043e\u043b\u043e\u0442\u00bb"}}]},{"title":"\u041c\u043e\u0441\u043a\u0432\u0430 \u0411\u0435\u043b\u043e\u0440\u0443\u0441\u0441\u043a\u0430\u044f <em>\u0411\u0435\u043b\u043e\u0440\u0443\u0441\u0441\u043a\u043e\u0435 \u043d\u0430\u043f\u0440.<\/em>","type":"railway","wagons":[{"id":"11","type":"railway","data":{"title":"\u041c\u043e\u0441\u043a\u0432\u0430 \u0411\u0435\u043b\u043e\u0440\u0443\u0441\u0441\u043a\u0430\u044f","group_id":"1190"}}]},{"title":"\u041c\u043e\u0441\u043a\u0432\u0430 \u041a\u0443\u0440\u0441\u043a\u0430\u044f <em>\u0413\u043e\u0440\u044c\u043a\u043e\u0432\u0441\u043a\u043e\u0435 \u043d\u0430\u043f\u0440.<\/em>","type":"railway","wagons":[{"id":"82","type":"railway","data":{"title":"\u041c\u043e\u0441\u043a\u0432\u0430 \u041a\u0443\u0440\u0441\u043a\u0430\u044f","group_id":"1192"}}]},{"title":"\u0421\u0435\u0440\u043f \u0438 \u041c\u043e\u043b\u043e\u0442 <em>\u0413\u043e\u0440\u044c\u043a\u043e\u0432\u0441\u043a\u043e\u0435 \u043d\u0430\u043f\u0440.<\/em>","type":"railway","wagons":[{"id":"83","type":"railway","data":{"title":"\u0421\u0435\u0440\u043f \u0438 \u041c\u043e\u043b\u043e\u0442","group_id":"1192"}}]}]';
     var methods = {
-        collapse: function(){
-            this.find('input.entry').prop('placeholder', '').css('width', 40);
+        // collapse: function(){
+        //     this.find('input.entry').prop('placeholder', '').css('width', 40);
 
-            return this;
-        },
+        //     return this;
+        // },
 
-        expand: function(){
-            this.find('input.entry').prop('placeholder', this.data("placeholder")).css('width', this.data("width"));
+        // expand: function(){
+        //     this.find('input.entry').prop('placeholder', this.data("placeholder")).css('width', this.data("width"));
 
-            return this;
-        },
+        //     return this;
+        // },
 
         getState: function(){
             return this.data('searchString').searchSelector.hasClass('active');
@@ -43,7 +43,7 @@
             $this.append(thisData.entryBlock).after(thisData.searchSelector);
 
             thisData.entryBlock.css('width', $this.data('width'));
-            thisData.entryBlock.autosizeInput();
+            //thisData.entryBlock.autosizeInput();
 
             function highlight(content, what) {
                 what = $.trim(what);
@@ -126,7 +126,7 @@
             thisData.entryBlock.on('keydown', function(e){
                 if(!$(this).val() && $.inArray(e.keyCode, [37,38,39,40,13,8]) < 0){
                     $(this).prop('placeholder', '');
-                    $(this).css('width', 40);
+                    // $(this).css('width', 40);
                 }
             });
 
@@ -184,7 +184,8 @@
 
                 e.stopPropagation();
                 thisData.searchSelector.removeClass('active');
-                thisData.entryBlock.val(value).css('width', thisData.entryBlock.parent().data('width'));
+                // thisData.entryBlock.val(value);.css('width', thisData.entryBlock.parent().data('width'))
+                thisData.entryBlock.val(value);
 
             });
         }
